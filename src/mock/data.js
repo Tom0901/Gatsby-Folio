e2d530d8e185e1d65591c219bd136dfdf43487ec
Tom0901 <50862075+Tom0901@users.noreply.github.com>
2020-10-04 12:52:09 +0100

    full project

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Jack Stevens', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
@@ -10,22 +10,49 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Tom Jackson',
+  subtitle: 'JS Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
+  paragraphOne: 'I am a JS developer.',
+  paragraphTwo: 'I build cool things with Gatsby.',
   paragraphThree: '',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'Project2.png',
+    title: 'Project 2 is an awesome slick Gatsby website',
+    info: 'I built it typing with my feet',
+    info2: '',
+    url: 'https://tom0901.github.io/React-Website/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Project1.png',
+    title: '',
+    info: '',
+    info2: '',
+    url: 'https://tom0901.github.io/React-Website/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
   {
     id: nanoid(),
     img: 'project.jpg',
